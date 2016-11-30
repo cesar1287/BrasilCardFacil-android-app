ff14624fee3d10cbd808c3b0e5b6c6debb2ff6ab
César Rodrigues <rasec1287@hotmail.com>
2016-11-30 20:24:42 -0300

    Adicionada MainActivity com NavigationDrawer

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/SignInActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/SignInActivity.java
@@ -1,7 +1,10 @@
 package br.com.brasilcardfacil.www.brasilcardfacil.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import br.com.brasilcardfacil.www.brasilcardfacil.R;
 
@@ -11,5 +14,15 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button btEntrar = (Button) findViewById(R.id.SignIn_entrar);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
