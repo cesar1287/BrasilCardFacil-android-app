906fb73e4bc6369642976d2ef1347c1691857b51
César Rodrigues <rasec1287@hotmail.com>
2016-12-08 23:02:10 -0300

    Atualizada tela de usuário

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/AccountActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/AccountActivity.java
@@ -2,16 +2,23 @@ package br.com.brasilcardfacil.www.brasilcardfacil.view;
 
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 import br.com.brasilcardfacil.www.brasilcardfacil.R;
 
@@ -43,5 +50,52 @@ public class AccountActivity extends AppCompatActivity {
         });
         TextView nav_nome = (TextView) findViewById(R.id.acc_name);
         nav_nome.setText(name);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        final EditText email = (EditText) findViewById(R.id.acc_email);
+        email.setText(user.getEmail());
+
+        final EditText phone = (EditText) findViewById(R.id.acc_phone);
+
+        final EditText born = (EditText) findViewById(R.id.acc_born);
+
+        final EditText sex = (EditText) findViewById(R.id.acc_sex);
+
+        final ImageButton btEmail = (ImageButton) findViewById(R.id.acc_edit1);
+        btEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email.setEnabled(true);
+                email.requestFocus();
+            }
+        });
+
+        ImageButton btPhone = (ImageButton) findViewById(R.id.acc_edit2);
+        btPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phone.setEnabled(true);
+                phone.requestFocus();
+            }
+        });
+
+        ImageButton btBorn = (ImageButton) findViewById(R.id.acc_edit3);
+        btBorn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                born.setEnabled(true);
+                born.requestFocus();
+            }
+        });
+
+        ImageButton btSex = (ImageButton) findViewById(R.id.acc_edit4);
+        btSex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sex.setEnabled(true);
+                sex.requestFocus();
+            }
+        });
     }
 }
