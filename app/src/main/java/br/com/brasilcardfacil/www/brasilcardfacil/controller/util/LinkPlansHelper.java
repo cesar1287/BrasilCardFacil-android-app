96d8c758d6ec462f12b3d0f1fd4466993c7cd55c
César Rodrigues <rasec1287@hotmail.com>
2016-12-11 19:05:18 -0300

    Adicionada classe para controle dos links de compra e WebViewActivity

---
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/controller/util/LinkPlansHelper.java
@@ -0,0 +1,8 @@
+package br.com.brasilcardfacil.www.brasilcardfacil.controller.util;
+
+/**
+ * Created by César on 11/12/2016.
+ */
+
+public class LinkPlansHelper {
+}
---
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/WebViewPlansActivity.java
@@ -0,0 +1,13 @@
+package br.com.brasilcardfacil.www.brasilcardfacil.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebViewPlansActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view_plans);
+    }
+}
