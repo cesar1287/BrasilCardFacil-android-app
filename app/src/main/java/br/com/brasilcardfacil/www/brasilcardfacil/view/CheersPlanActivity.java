f00305a76a53f0da2bf9fbed15b45821b1214abc
César Rodrigues <rasec1287@hotmail.com>
2016-12-22 02:12:01 -0300

    Adicionada activity para o plano Saúde

---
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/CheersPlanActivity.java
@@ -0,0 +1,30 @@
+package br.com.brasilcardfacil.www.brasilcardfacil.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import br.com.brasilcardfacil.www.brasilcardfacil.R;
+import br.com.brasilcardfacil.www.brasilcardfacil.controller.util.LinkPlansHelper;
+
+public class CheersPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheers_plan);
+
+        ImageButton btBuy = (ImageButton) findViewById(R.id.cheers_plan_button_buy);
+        btBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent diamond_plan = new Intent(CheersPlanActivity.this, WebViewPlansActivity.class);
+                diamond_plan.putExtra("link", LinkPlansHelper.cheers_plan);
+                startActivity(diamond_plan);
+            }
+        });
+    }
+}
