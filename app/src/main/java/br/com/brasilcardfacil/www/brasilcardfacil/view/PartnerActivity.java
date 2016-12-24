2c094157e1026f0ac2ba13d633227cd82f820d56
César Rodrigues <rasec1287@hotmail.com>
2016-12-23 21:06:58 -0300

    Atualizado manifest e activity principal de parceiros para inclusão das telas que detalham os parceiros

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/PartnerActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/PartnerActivity.java
@@ -1,7 +1,10 @@
 package br.com.brasilcardfacil.www.brasilcardfacil.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import br.com.brasilcardfacil.www.brasilcardfacil.R;
 
@@ -11,5 +14,61 @@ public class PartnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partner);
+
+        ImageButton btClinicHosp = (ImageButton) findViewById(R.id.partner_med_hosp);
+        btClinicHosp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PartnerActivity.this, ClinicHospitalPartnerActivity.class));
+            }
+        });
+
+        ImageButton btOdonto = (ImageButton) findViewById(R.id.partner_odonto);
+        btOdonto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PartnerActivity.this, DentistryPartnerActivity.class));
+            }
+        });
+
+        ImageButton btVeteri = (ImageButton) findViewById(R.id.partner_veteri);
+        btVeteri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PartnerActivity.this, VeterinaryPartnerActivity.class));
+            }
+        });
+
+        ImageButton btComer = (ImageButton) findViewById(R.id.partner_comercio);
+        btComer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PartnerActivity.this, TradePartnerActivity.class));
+            }
+        });
+
+        ImageButton btLab = (ImageButton) findViewById(R.id.partner_lab);
+        btLab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PartnerActivity.this, LaboratoryPartnerActivity.class));
+            }
+        });
+
+        ImageButton btAdvo = (ImageButton) findViewById(R.id.partner_advocacia);
+        btAdvo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PartnerActivity.this, AdvocacyPartnerActivity.class));
+            }
+        });
+
+        ImageButton btFuneral = (ImageButton) findViewById(R.id.partner_funeraria);
+        btFuneral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PartnerActivity.this, FuneralPartnerActivity.class));
+            }
+        });
     }
 }
