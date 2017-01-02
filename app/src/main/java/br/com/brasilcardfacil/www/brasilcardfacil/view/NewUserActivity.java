20cadf15e5ef679140bf5dc39be804d19857d77b
César Rodrigues <rasec1287@hotmail.com>
2017-01-02 11:15:44 -0300

    Excluido imports redundantes

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/NewUserActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/NewUserActivity.java
@@ -16,7 +16,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
