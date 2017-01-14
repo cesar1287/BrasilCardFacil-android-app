10e358e9f6247027e9e378866a318f5c87835acf
César Rodrigues <rasec1287@hotmail.com>
2017-01-14 16:11:41 -0300

    Adicionada classe DAO para uso em features a serem adicionadas no futuro

---
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/model/BrasilCardFacilDAO.java
@@ -0,0 +1,8 @@
+package br.com.brasilcardfacil.www.brasilcardfacil.model;
+
+/**
+ * Created by César on 14/01/2017.
+ */
+
+public class BrasilCardFacilDAO {
+}
--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/model/ModelClass.java
+++
@@ -1,4 +0,0 @@
-package br.com.brasilcardfacil.www.brasilcardfacil.model;
-
-public class ModelClass {
-}
