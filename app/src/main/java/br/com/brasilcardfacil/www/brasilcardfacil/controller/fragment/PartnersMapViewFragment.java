30d9d3a8bb03280954f8efaf24a45cd6cf910e04
César Rodrigues <rasec1287@hotmail.com>
2017-01-18 20:34:37 -0300

    Atualizada classe de mapview para utilização pela tela de Ofertas

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/controller/fragment/PartnersMapViewFragment.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/controller/fragment/PartnersMapViewFragment.java
@@ -2,6 +2,10 @@ package br.com.brasilcardfacil.www.brasilcardfacil.controller.fragment;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -27,13 +31,17 @@ import java.util.List;
 import br.com.brasilcardfacil.www.brasilcardfacil.R;
 import br.com.brasilcardfacil.www.brasilcardfacil.controller.domain.Partner;
 
-public class PartnersMapViewFragment extends Fragment implements ActivityCompat.OnRequestPermissionsResultCallback{
+import static android.content.Context.LOCATION_SERVICE;
+
+public class PartnersMapViewFragment extends Fragment implements ActivityCompat.OnRequestPermissionsResultCallback, LocationListener{
 
     MapView mMapView;
     private GoogleMap googleMap;
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     List<Partner> partners = new ArrayList<>();
+
+    boolean showed = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -60,8 +68,6 @@ public class PartnersMapViewFragment extends Fragment implements ActivityCompat.
                 googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
                 enableMyLocation();
-
-                setupUI();
             }
         });
 
@@ -77,10 +83,27 @@ public class PartnersMapViewFragment extends Fragment implements ActivityCompat.
         } else if (googleMap != null) {
             // Access to the location has been granted to the app.
             googleMap.setMyLocationEnabled(true);
+
+            // Getting LocationManager object from System Service LOCATION_SERVICE
+            LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+
+            // Creating a criteria object to retrieve provider
+            Criteria criteria = new Criteria();
+
+            // Getting the name of the best provider
+            String provider = locationManager.getBestProvider(criteria, true);
+
+            // Getting Current Location
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            if(location!=null){
+                onLocationChanged(location);
+            }
+            locationManager.requestLocationUpdates(provider, 20000, 0, this);
         }
     }
 
-    public void setupUI(){
+    public void setupUI(LatLng latLng){
         assert partners != null;
         for (Partner p: partners){
             // For dropping a marker at a point on the Map
@@ -88,10 +111,8 @@ public class PartnersMapViewFragment extends Fragment implements ActivityCompat.
             googleMap.addMarker(new MarkerOptions().position(address).title(p.getName()).snippet(p.getName()));
         }
 
-        LatLng bh = new LatLng(-19.9245, -44.0053);
-
         // For zooming automatically to the location of the marker
-        CameraPosition cameraPosition = new CameraPosition.Builder().target(bh).zoom(11.45f).build();
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(latLng).zoom(14.45f).build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
@@ -138,5 +159,39 @@ public class PartnersMapViewFragment extends Fragment implements ActivityCompat.
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if(showed) {
+            // Getting latitude of the current location
+            double latitude = location.getLatitude();
+
+            // Getting longitude of the current location
+            double longitude = location.getLongitude();
+
+            // Creating a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            setupUI(latLng);
+
+            showed = false;
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
