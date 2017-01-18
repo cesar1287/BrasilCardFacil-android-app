2d534741ce6c4c180a7e9a3c509ac6529bed879a
César Rodrigues <rasec1287@hotmail.com>
2017-01-18 20:12:13 -0300

    Atualizada tela de aviso ao usuário sobre os planos

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/WarningActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/WarningActivity.java
@@ -1,7 +1,10 @@
 package br.com.brasilcardfacil.www.brasilcardfacil.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import br.com.brasilcardfacil.www.brasilcardfacil.R;
 
@@ -11,5 +14,21 @@ public class WarningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warning);
+
+        ImageButton btOk = (ImageButton) findViewById(R.id.warning_ok);
+        btOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WarningActivity.this, PlansActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        startActivity(new Intent(this, PlansActivity.class));
     }
 }
