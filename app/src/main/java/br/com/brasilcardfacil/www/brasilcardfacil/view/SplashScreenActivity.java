ef35ff457581263a7ee077b7454a8fb241d3ba3f
César Rodrigues <rasec1287@hotmail.com>
2017-01-24 12:00:40 -0300

    Atualizado splash screen em caso de notificações quando o aplicativo está em segundo plano

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/SplashScreenActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/SplashScreenActivity.java
@@ -9,18 +9,47 @@ import br.com.brasilcardfacil.www.brasilcardfacil.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    boolean showed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-               startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-                finish();
+        if (getIntent().getExtras() != null) {
+
+            for (String key : getIntent().getExtras().keySet()) {
+                String value = getIntent().getExtras().getString(key);
+
+                if (key.equals("type_notification") && value.equals("active health")) {
+                    startActivity(new Intent(this, ActiveHealthActivity.class));
+                    showed = true;
+                    finish();
+                    break;
+                }else if(key.equals("type_notification") && value.equals("partner")){
+                    startActivity(new Intent(this, PartnerActivity.class));
+                    showed = true;
+                    finish();
+                    break;
+                }else if(key.equals("type_notification") && value.equals("offer")){
+                    startActivity(new Intent(this, OfferActivity.class));
+                    showed = true;
+                    finish();
+                    break;
+                }
             }
-        }, 3000);
+        }
+
+        if(!showed) {
+            setContentView(R.layout.activity_splash_screen);
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, 3000);
+        }
     }
 }
