b1c4504fc18c19d2b6a19d667396944e599e8ebc
César Rodrigues <rasec1287@hotmail.com>
2017-01-25 15:55:46 -0300

    Pequeno ajuste na classe de links dos planos

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/controller/util/LinkPlansHelper.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/controller/util/LinkPlansHelper.java
@@ -3,6 +3,6 @@ package br.com.brasilcardfacil.www.brasilcardfacil.controller.util;
 public class LinkPlansHelper {
 
     public static final String cheers_plan = "http://www.brasilcardfacil.com.br/page69.html";
-    public static final String total_plan = "http://www.brasilcardfacil.com.br/page68.html";
+    public static final String total_plan = "http://brasilcardfacil.com.br/page68.html";
     public static final String funeral_plan = "http://www.brasilcardfacil.com.br/page70.html";
 }
