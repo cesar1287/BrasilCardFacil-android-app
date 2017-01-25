aeda29fc08b97409df8d9b0c42ee3bab1ade8374
César Rodrigues <rasec1287@hotmail.com>
2017-01-25 15:44:07 -0300

    Atualizada tela de detalhes da saúde ativa para exibição a partir das notificações

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/ActiveHealthDetailsActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/ActiveHealthDetailsActivity.java
@@ -1,27 +1,108 @@
 package br.com.brasilcardfacil.www.brasilcardfacil.view;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.brasilcardfacil.www.brasilcardfacil.R;
 import br.com.brasilcardfacil.www.brasilcardfacil.controller.domain.ActiveHealth;
+import br.com.brasilcardfacil.www.brasilcardfacil.controller.domain.ActiveHealthNotification;
 
 public class ActiveHealthDetailsActivity extends AppCompatActivity {
 
     ActiveHealth activeHealth = new ActiveHealth();
+
+    String db, child;
+
+    Query active_health_notification;
+
+    ValueEventListener valueEventListener;
+    ValueEventListener singleValueEventListener;
+
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_health_details);
 
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        db = getIntent().getStringExtra("db");
+        child = getIntent().getStringExtra("child");
+
         activeHealth = (ActiveHealth) getIntent().getSerializableExtra("active_health");
 
-        setupUI();
+        if(db != null & child != null){
+
+            dialog = ProgressDialog.show(this,"", this.getResources().getString(R.string.loading_active_health_pls_wait), true, false);
+            active_health_notification = mDatabase.child(db).child(child);
+
+            loadActiveHealthNotification();
+        }else{
+            setupUI();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(active_health_notification!=null) {
+            active_health_notification.removeEventListener(valueEventListener);
+            active_health_notification.removeEventListener(singleValueEventListener);
+        }
+    }
+
+    private void loadActiveHealthNotification() {
+
+        valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                ActiveHealthNotification activeHealthNotification = dataSnapshot.getValue(ActiveHealthNotification.class);
+                activeHealth = new ActiveHealth();
+
+                activeHealth.setTitle(activeHealthNotification.title);
+                activeHealth.setDescription(activeHealthNotification.description);
+                activeHealth.setUrlBanner(activeHealthNotification.url_banner);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(ActiveHealthDetailsActivity.this, R.string.error_loading_active_health, Toast.LENGTH_LONG).show();
+                finish();
+            }
+        };
+
+        singleValueEventListener = new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                setupUI();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(ActiveHealthDetailsActivity.this, R.string.error_loading_active_health, Toast.LENGTH_LONG).show();
+                finish();
+            }
+        };
+
+        active_health_notification.addValueEventListener(valueEventListener);
+
+        active_health_notification.addListenerForSingleValueEvent(singleValueEventListener);
     }
 
     private void setupUI() {
