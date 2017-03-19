cb56f1da9bd40a42ec321399aa608a552b905e2d
César Rodrigues <rasec1287@hotmail.com>
2017-03-19 13:24:28 -0300

    Ajustada tela do segmento Turismo

--- app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/PartnerTourismActivity.java
+++ app/src/main/java/br/com/brasilcardfacil/www/brasilcardfacil/view/PartnerTourismActivity.java
@@ -1,9 +1,13 @@
 package br.com.brasilcardfacil.www.brasilcardfacil.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import br.com.brasilcardfacil.www.brasilcardfacil.R;
+import br.com.brasilcardfacil.www.brasilcardfacil.controller.util.Utility;
 
 public class PartnerTourismActivity extends AppCompatActivity {
 
@@ -11,5 +15,65 @@ public class PartnerTourismActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partner_tourism);
+
+        setupListeners();
+    }
+
+    private void setupListeners() {
+
+        ImageButton btTravelAgency = (ImageButton) findViewById(R.id.tourism_travel_agency);
+        btTravelAgency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PartnerTourismActivity.this, PartnerCategoryActivity.class);
+                intent.putExtra(Utility.CATEGORY, Utility.TOURISM);
+                intent.putExtra(Utility.SUBCATEGORY, Utility.TOURISM_SUBCATEGORY_TRAVEL_AGENCY);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton btGuide = (ImageButton) findViewById(R.id.tourism_guide);
+        btGuide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PartnerTourismActivity.this, PartnerCategoryActivity.class);
+                intent.putExtra(Utility.CATEGORY, Utility.TOURISM);
+                intent.putExtra(Utility.SUBCATEGORY, Utility.TOURISM_SUBCATEGORY_GUIDE);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton btHostel = (ImageButton) findViewById(R.id.tourism_hostel);
+        btHostel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PartnerTourismActivity.this, PartnerCategoryActivity.class);
+                intent.putExtra(Utility.CATEGORY, Utility.TOURISM);
+                intent.putExtra(Utility.SUBCATEGORY, Utility.TOURISM_SUBCATEGORY_HOSTEL);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton btHotel = (ImageButton) findViewById(R.id.tourism_hotel);
+        btHotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PartnerTourismActivity.this, PartnerCategoryActivity.class);
+                intent.putExtra(Utility.CATEGORY, Utility.TOURISM);
+                intent.putExtra(Utility.SUBCATEGORY, Utility.TOURISM_SUBCATEGORY_HOTEL);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton btTransport = (ImageButton) findViewById(R.id.tourism_transport);
+        btTransport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PartnerTourismActivity.this, PartnerCategoryActivity.class);
+                intent.putExtra(Utility.CATEGORY, Utility.TOURISM);
+                intent.putExtra(Utility.SUBCATEGORY, Utility.TOURISM_SUBCATEGORY_TRANSPORT);
+                startActivity(intent);
+            }
+        });
     }
 }
